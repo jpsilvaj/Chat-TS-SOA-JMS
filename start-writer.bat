958eec7819ee5968a9aa960560a8ea11409649aa
João Paulo da Silva Jùnior <jpsilvaj@gmail.com>
2015-04-21 20:25:38 -0300

    Add scripts

---
+++ start-writer.bat
@@ -0,0 +1,1 @@
+java -cp libs\jini-core.jar;libs\jini-ext.jar;libs\reggie.jar;libs\outrigger.jar;. C:\sandbox\Chat-TS-SOA-JMS\out\production\Chat-TS-SOA-JMS\br\edu\ifce\chattssoajms\controller\WriteMessage
